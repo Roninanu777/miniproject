89afb697b88364ec67a830db30304e8d466e22a6
@Roninanu777 <ronirajkamalpradhan1112@gmail.com>
2020-12-25 18:11:51 +0530

    adding routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,39 @@
+import React, { useState } from "react";
+import Modal from "./components/Modal";
+import PurchaseDetails from "./components/PurchaseDetails";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [status, setStatus] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  const postData = () => {};
+
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            {status && <Modal closeModal={() => setStatus(false)} />}
+            <div className="container">
+              <button id="modal-trigger" onClick={() => setStatus(true)}>
+                Open Modal
+              </button>
+            </div>
+          </Route>
+          <Route exact path="/purchase-details">
+            <PurchaseDetails />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+  // return (
+  //   <div className="App">
+  //     <PurchaseDetails />
+  //   </div>
+  // );
 }
 
 export default App;
